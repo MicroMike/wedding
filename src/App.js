c8d77d7270274874bfb920c53e812a19094fd45d
Mike <assoune.mike@gmail.com>
2020-08-21 11:59:35 +0200

    text

--- src/App.js
+++ src/App.js
@@ -31,17 +31,6 @@ function App() {
       Shanael & Kevin<br />
       15, Boulevard Kennedy, 94000 Créteil<br />
       06.11.25.02.13 ou 06.23.25.50.99<br />
-      Vous invitent à vous joindre à leur mariage<br />
-      <br />
-      Le 16 septembre 2020 à 17h<br />
-      Pour la houppa<br />
-      (la cérémonie religieuse)<br />
-      Suivie de la réception au château de santeny<br />
-      2 route de marolles, 94440 Santeny<br />
-      <br />
-      Shanael & Kevin<br />
-      15, Boulevard Kennedy, 94000 Créteil<br />
-      06.11.25.02.13 ou 06.23.25.50.99
 
       <div className="btns">
         <button>Nous viendrons</button>
